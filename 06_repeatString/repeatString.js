fa4e94be16c146bafd02fa7d3f8be7e4c31bf5db
Bao-Trinh-Quoc <bao.trinhquoc@hcmut.edu.vn>
2025-06-03 15:09:34 +0700

    Finished 06

--- 06_repeatString/repeatString.js
+++ 06_repeatString/repeatString.js
@@ -1,5 +1,13 @@
-const repeatString = function() {
+const repeatString = function(string, time) {
+    if (time < 0) {
+        return 'ERROR';
+    }
+    let res = '';
+    for (let i = 0; i < time; i++) {
+        res += string;
+    }
 
+    return res;
 };
 
 // Do not edit below this line
