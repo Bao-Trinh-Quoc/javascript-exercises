ca85e0d4b1b2fb0dc5efd1f89ed6c3813aab9ea2
Bao-Trinh-Quoc <bao.trinhquoc@hcmut.edu.vn>
2025-06-04 18:47:44 +0700

    finished 07

--- 07_reverseString/reverseString.js
+++ 07_reverseString/reverseString.js
@@ -1,5 +1,13 @@
-const reverseString = function() {
+const reverseString = function(str) {
+    let res = "";
+    for (let i = str.length - 1; i >= 0; i--) {
+        res += str[i];
+    }
+    return res;
 
+    // harness the power of method
+    // split str -> arr then reverse then join -> str
+    // str.split("").reverse().join("");
 };
 
 // Do not edit below this line
